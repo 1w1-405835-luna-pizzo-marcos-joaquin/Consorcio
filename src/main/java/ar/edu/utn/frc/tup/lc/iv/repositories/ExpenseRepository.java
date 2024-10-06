38e16c3c99512b9b99c077e1780be45ecd45e76b
Santino Consigli <406030@tecnicatura.frc.utn.edu.ar>
2024-10-06 12:05:05 -0300

    fix bug in notation ExpenseRepository

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/repositories/ExpenseRepository.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/repositories/ExpenseRepository.java
@@ -2,8 +2,9 @@ package ar.edu.utn.frc.tup.lc.iv.repositories;
 
 import ar.edu.utn.frc.tup.lc.iv.entities.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
 public interface ExpenseRepository extends JpaRepository<ExpenseEntity,Integer> {
 }
