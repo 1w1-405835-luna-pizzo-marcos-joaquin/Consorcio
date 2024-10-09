acd9e11ea0bb0d0853b4405d783f0e7534f62502
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-10-09 19:05:18 -0300

    integrated controller with bill service

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/controllers/BillExpenseController.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/controllers/BillExpenseController.java
@@ -1,7 +1,10 @@
 package ar.edu.utn.frc.tup.lc.iv.controllers;
 
-import ar.edu.utn.frc.tup.lc.iv.dtos.billExpense.BillExpenseDto;
+import ar.edu.utn.frc.tup.lc.iv.dtos.billExpense.response.BillExpenseDto;
 import ar.edu.utn.frc.tup.lc.iv.dtos.billExpense.PeriodDto;
+import ar.edu.utn.frc.tup.lc.iv.services.impl.BillExpenseService;
+import ar.edu.utn.frc.tup.lc.iv.services.interfaces.IBillExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/billexpenses")
 public class BillExpenseController {
+    private final IBillExpenseService billExpenseService;
+    @Autowired
+    public BillExpenseController(BillExpenseService billExpenseService) {
+        this.billExpenseService = billExpenseService;
+    }
     @PostMapping("/generate")
     public ResponseEntity<BillExpenseDto> generateExpenses(@RequestBody(required = true)PeriodDto periodDto){
-        BillExpenseDto result = new BillExpenseDto();
+        BillExpenseDto result = billExpenseService.generateBillExpense(periodDto);
         return ResponseEntity.ok(result);
     }
 }
