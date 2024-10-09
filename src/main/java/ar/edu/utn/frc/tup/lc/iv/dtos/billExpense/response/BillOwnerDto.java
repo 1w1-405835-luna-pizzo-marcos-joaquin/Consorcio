8b5a01f353bae0cd1cd42254255d38d9566e59e5
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-10-09 19:00:01 -0300

    add lombok

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/billExpense/response/BillOwnerDto.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/billExpense/response/BillOwnerDto.java
@@ -1,9 +1,14 @@
 package ar.edu.utn.frc.tup.lc.iv.dtos.billExpense.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BillOwnerDto {
     @JsonProperty("owner_id")
     private Integer id;
