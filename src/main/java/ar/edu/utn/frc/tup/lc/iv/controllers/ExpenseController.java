b165147b6432cfd78833aa8f08c2c6cccd088452
1w1-405835-luna-pizzo-marcos-joaquin <142423545+1w1-405835-luna-pizzo-marcos-joaquin@users.noreply.github.com>
2024-10-11 18:31:06 -0300

    cambios locales

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/controllers/ExpenseController.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/controllers/ExpenseController.java
@@ -1,18 +1,23 @@
 package ar.edu.utn.frc.tup.lc.iv.controllers;
 
+import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoExpenseQuery;
 import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoRequestExpense;
 import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoResponseExpense;
 import ar.edu.utn.frc.tup.lc.iv.services.impl.ExpenseService;
 import ar.edu.utn.frc.tup.lc.iv.services.interfaces.IExpenseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +34,28 @@ public class ExpenseController {
             @RequestPart(value = "file", required = false) MultipartFile file) {
         return expenseService.postExpense(request, file);
     }
+
+    @GetMapping("/getById")
+    @Operation(summary = "Get expense by id")
+    @ApiResponse(responseCode = "200", description = "Expenses get successfully",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = DtoExpenseQuery.class)))
+    public ResponseEntity<DtoExpenseQuery> getExpenseById(@RequestParam (required = true)int expenseId) {
+        return ResponseEntity.ok(expenseService.getExpenseById(expenseId)) ;
+    }
+
+    @GetMapping("/getByFilters")
+    @Operation(summary = "Get expenses by filters")
+    @ApiResponse(responseCode = "200", description = "Expenses get successfully",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = DtoExpenseQuery.class)))
+    public ResponseEntity<List <DtoExpenseQuery>> getExpenses(@RequestParam (required = false)String expenseType,
+                                                              @RequestParam (required = false)String category,
+                                                              @RequestParam (required = false)String provider,
+                                                              @RequestParam (required = false)String dateFrom,
+                                                              @RequestParam (required = false)String dateTo) {
+        return ResponseEntity.ok(expenseService.getExpenses(expenseType, category, provider, dateFrom, dateTo)) ;
+    }
+
 
 }
--- src/main/java/ar/edu/utn/frc/tup/lc/iv/services/interfaces/IExpenseService.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/services/interfaces/IExpenseService.java
@@ -1,11 +1,16 @@
 package ar.edu.utn.frc.tup.lc.iv.services.interfaces;
 
+import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoExpenseQuery;
 import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoRequestExpense;
 import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoResponseExpense;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Service
 public interface IExpenseService {
     DtoResponseExpense postExpense(DtoRequestExpense request,MultipartFile file);
+    public DtoExpenseQuery getExpenseById(Integer expenseId);
+    public List<DtoExpenseQuery> getExpenses(String expenseType, String category, String provider, String dateFrom, String dateTo);
 }
