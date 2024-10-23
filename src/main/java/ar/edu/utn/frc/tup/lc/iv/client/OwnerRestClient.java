b18492c4d8e2f10479fbd3bcaa7cbf5b664cbc1a
EbeltramoUtn <166845082+EbeltramoUtn@users.noreply.github.com>
2024-10-23 16:34:51 -0300

    added environment variables for apis url (#17)

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/client/OwnerRestClient.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/client/OwnerRestClient.java
@@ -4,6 +4,7 @@ package ar.edu.utn.frc.tup.lc.iv.client;
 import ar.edu.utn.frc.tup.lc.iv.controllers.manageExceptions.CustomException;
 import ar.edu.utn.frc.tup.lc.iv.dtos.owner.OwnerDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,8 @@ import java.util.List;
 @Service
 public class OwnerRestClient {
     private final RestTemplate restTemplate;
-    private static final String ROOT_URL = "https://my-json-server.typicode.com/EbeltramoUtn/demoTP";
+    @Value("${app.api-owner}")
+    private String ROOT_URL;
     @Autowired
     public OwnerRestClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
