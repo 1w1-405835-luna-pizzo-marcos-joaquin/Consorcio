fffa721026becc8389ace85b05f4353167f8d1ad
Brandonloz3 <Baplozano@gmail.com>
2024-10-24 23:15:06 -0300

    Arreglo categorias

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/services/impl/ExpenseCategoryService.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/services/impl/ExpenseCategoryService.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frc.tup.lc.iv.services.impl;
 
 import ar.edu.utn.frc.tup.lc.iv.controllers.manageExceptions.CustomException;
 import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoCategory;
+import ar.edu.utn.frc.tup.lc.iv.dtos.common.DtoExpenseQuery;
 import ar.edu.utn.frc.tup.lc.iv.entities.ExpenseCategoryEntity;
 import ar.edu.utn.frc.tup.lc.iv.models.ExpenseCategoryModel;
 import ar.edu.utn.frc.tup.lc.iv.repositories.ExpenseCategoryRepository;
@@ -33,7 +34,21 @@ public class ExpenseCategoryService implements IExpenseCategoryService {
         if ( Collections.emptyList().equals(expenseCategoryEntities) ) {
             throw new CustomException("No categories found", HttpStatus.NOT_FOUND);
         }
-        return expenseCategoryEntities.stream().map(categoryEntity -> modelMapper.map(categoryEntity, DtoCategory.class)).toList();
+        DtoCategory dtoCategory = new DtoCategory();
+        List<DtoCategory> dtoCategories = new ArrayList<>();
+        for (ExpenseCategoryEntity categoryEntity : expenseCategoryEntities) {
+            dtoCategory.setId(categoryEntity.getId());
+            dtoCategory.setDescription(categoryEntity.getDescription());
+            dtoCategory.setLastUpdatedDatetime(categoryEntity.getLastUpdatedDatetime());
+            if (categoryEntity.getEnabled()){
+                dtoCategory.setState("Activo");
+            } else {
+                dtoCategory.setState("Inactivo");
+            }
+            dtoCategories.add(dtoCategory);
+        }
+
+        return dtoCategories;
     }
 
 }
